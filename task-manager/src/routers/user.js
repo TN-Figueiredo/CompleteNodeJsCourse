f6efcfcd7f654f625ed717b999c7c9bfdcbd0ca9
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-08-16 18:06:24 -0300

    feat: file uploads

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../models/user");
 const auth = require("../middlewares/auth");
+const multer = require("multer");
+const sharp = require('sharp');
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -96,6 +98,61 @@ router.delete("/users/me", auth, async (req, res) => {
     return res.status(200).send(req.user);
   } catch (e) {
     res.status(500).send(e);
+  }
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  dest: "avatars",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload a image file"));
+    }
+
+    cb(undefined, true);
+
+    // cb(new Error('File must be a PDF'))
+    // cb(undefined, true)
+    // cb(undefined, false)
+  },
+  storage,
+});
+
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.status(200).send();
+  },
+  (error, _, res, _2) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.status(200).send();
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
   }
 });
 
